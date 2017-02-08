f31dafdb380d03085850a385e26bc551ff03f228
4015c3 <4015c3@grosella.ece.uprm.edu>
2017-02-08 12:28:18 -0400

    Modified frame's title, size, and centered it on display.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,11 +2,13 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+		
+		JFrame myFrame = new JFrame("Amy Ayala Ramos");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(800, 400);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
-		myFrame.setTitle("Amy Ayala Ramos");
+		//myFrame.setTitle("Amy Ayala Ramos");
+		
 	}
 }
